07290aa4fff1e8bfc7a29cd9d0749c655002f215
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-10-10 19:32:03 +0900

    refactor: refactor and asyncronize functions

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,32 +1,51 @@
+async function main() {
+  try {
+    const userId = getUserId();
+    const userInfo = await fetchUserInfo(userId);
+    const view = await createView(userInfo);
+    displayView(view);
+  } catch (error) {
+    console.error(`エラーが発生しました: (${error})`);
+  }
+}
+
 // userInfoのフェッチ
 function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-    .then((response) => {
-      console.log(response.status);
-      if (!response.ok) {
-        console.error('エラーレスポンス', response);
-      } else {
-        return response.json().then((userInfo) => {
-          // タグつきテンプレートリテラルでhtmlを生成する
-          const view = escapeHTML`
-          <h4>${userInfo.name} (@${userInfo.login})</h4>
-          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-          <dl>
-            <dt>Location</dt>
-            <dd>${userInfo.location}</dd>
-            <dt>Repositories</dt>
-            <dd>${userInfo.public_repos}</dd>
-          </dl>
-          `;
-          // HTMLを挿入する
-          const result = document.getElementById('result');
-          result.innerHTML = view;
-        });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  return fetch(
+    `https://api.github.com/users/${encodeURIComponent(userId)}`
+  ).then((response) => {
+    if (!response.ok) {
+      return Promise.reject(
+        new Error(`${response.status}: ${response.statusText}`)
+      );
+    } else {
+      return response.json();
+    }
+  });
+}
+
+function getUserId() {
+  return document.getElementById('userId').value;
+}
+
+function createView(userInfo) {
+  // タグつきテンプレートリテラルでhtmlを生成する
+  return escapeHTML`
+  <h4>${userInfo.name} (@${userInfo.login})</h4>
+  <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+  <dl>
+    <dt>Location</dt>
+    <dd>${userInfo.location}</dd>
+    <dt>Repositories</dt>
+    <dd>${userInfo.public_repos}</dd>
+  </dl>
+  `;
+}
+
+function displayView(view) {
+  // HTMLを挿入する
+  const result = document.getElementById('result');
+  result.innerHTML = view;
 }
 
 // エスケープ処理
