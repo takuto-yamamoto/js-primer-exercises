6380f2de218801d930eeaa5a1a32534a34920408
takuto-yamamoto <tyguitar0v0627@gmail.com>
2023-10-12 10:22:51 +0900

    add: feature to create todo item

--- todoapp/index.js
+++ todoapp/index.js
@@ -3,3 +3,4 @@ import { App } from './src/App.js';
 console.log('index.js: loaded');
 
 const app = new App();
+app.mount();
---
+++ todoapp/src/EventEmitter.js
@@ -0,0 +1,30 @@
+export class EventEmitter {
+  #listeners = new Map();
+  addEventListener(type, listener) {
+    if (!this.#listeners.has(type)) {
+      this.#listeners.set(type, new Set());
+    }
+    const listenerSet = this.#listeners.get(type);
+    listenerSet.add(listener);
+  }
+  emit(type) {
+    const listenerSet = this.#listeners.get(type);
+    if (!listenerSet) {
+      return;
+    }
+    listenerSet.forEach((listener) => {
+      listener.call(this);
+    });
+  }
+  removeEventLister(type, listener) {
+    const listenerSet = this.#listeners.get(type);
+    if (!listenerSet) {
+      return;
+    }
+    listenerSet.forEach((ownListener) => {
+      if (ownListener === listener) {
+        listenerSet.delete(listener);
+      }
+    });
+  }
+}
